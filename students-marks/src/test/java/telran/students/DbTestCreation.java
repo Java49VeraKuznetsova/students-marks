add23cf46b31dd3d29ff0568f142352b29872954
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2024-01-07 18:02:40 +0200

    HW #124_73 Yuri

--- students-marks/src/test/java/telran/students/DbTestCreation.java
+++ students-marks/src/test/java/telran/students/DbTestCreation.java
@@ -1,10 +1,11 @@
 package telran.students;
+
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
-import org.springframework.stereotype.*;
+import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 import telran.students.repo.StudentRepo;
@@ -14,99 +15,100 @@ import telran.students.model.*;
 @Component
 @RequiredArgsConstructor
 public class DbTestCreation {
-final StudentRepo studentRepo;
-final static long ID_1 = 1l;
-final static String NAME_1 = "name1";
-final static String PHONE_1 = "051-1234567";
-final static long ID_2 = 2l;
-final static String NAME_2 = "name2";
-final static String PHONE_2 = "052-1234567";
-final static long ID_3 = 3l;
-final static String NAME_3 = "name3";
-final static String PHONE_3 = "053-1234567";
-final static long ID_4 = 4l;
-final static String NAME_4 = "name4";
-final static String PHONE_4 = "054-1234567";
-final static long ID_5 = 5l;
-final static String NAME_5 = "name5";
-final static String PHONE_5 = "055-1234567";
-final static long ID_6 = 6l;
-final static String NAME_6 = "name6";
-final static String PHONE_6 = "056-1234567";
-final static long ID_7 = 7l;
-final static String NAME_7 = "name7";
-final static String PHONE_7 = "057-1234567";
-final static String SUBJECT_1 = "subject1";
-final static String SUBJECT_2 = "subject2";
-final static String SUBJECT_3 = "subject3";
-final static String SUBJECT_4 = "subject4";
-private static final LocalDate DATE_1 = LocalDate.parse("2023-10-20");
-private static final LocalDate DATE_2 = LocalDate.parse("2023-11-20");
-private static final LocalDate DATE_3 = LocalDate.parse("2023-12-20");
-private static final LocalDate DATE_4 = LocalDate.parse("2024-01-01");
-
-static Student[] students = {
-		new Student(ID_1, NAME_1, PHONE_1),
-		new Student(ID_2, NAME_2, PHONE_2),
-		new Student(ID_3, NAME_3, PHONE_3),
-		new Student(ID_4, NAME_4, PHONE_4),
-		new Student(ID_5, NAME_5, PHONE_5),
-		new Student(ID_6, NAME_6, PHONE_6),
-		new Student(ID_7, NAME_7, PHONE_7),
-};
-Mark[][] marks = {
-		{
-			new Mark(SUBJECT_1, DATE_1, 80 ),
-			new Mark(SUBJECT_1, DATE_2, 90 ),
-			new Mark(SUBJECT_2, DATE_2, 70 ),
-		},
-		{
-			new Mark(SUBJECT_3, DATE_1, 70 ),
-		},
-		{
-			new Mark(SUBJECT_1, DATE_1, 80 ),
-			new Mark(SUBJECT_4, DATE_3, 70 ),
-		},
-		{
-			new Mark(SUBJECT_2, DATE_1, 100 ),
-			new Mark(SUBJECT_3, DATE_3, 90 ),
-			new Mark(SUBJECT_4, DATE_4, 90 ),
-		},
-		{
-			new Mark(SUBJECT_1, DATE_1, 70 ),
-			new Mark(SUBJECT_3, DATE_3, 70 ),
-			
-		},
-		{
-			new Mark(SUBJECT_1, DATE_1, 100 ),
-			new Mark(SUBJECT_2, DATE_2, 100 ),
-			new Mark(SUBJECT_3, DATE_3, 100 ),
-			new Mark(SUBJECT_4, DATE_4, 100 ),
-		},
+	final StudentRepo studentRepo;
+	final static long ID_1 = 1l;
+	final static String NAME_1 = "name1";
+	final static String PONE_1 = "051-1234567";
+	final static long ID_2 = 2l;
+	final static String NAME_2 = "name2";
+	final static String PONE_2 = "052-1234567";
+	final static long ID_3 = 3l;
+	final static String NAME_3 = "name3";
+	final static String PONE_3 = "053-1234567";
+	final static long ID_4 = 4l;
+	final static String NAME_4 = "name4";
+	final static String PONE_4 = "054-1234567";
+	final static long ID_5 = 5l;
+	final static String NAME_5 = "name5";
+	final static String PONE_5 = "055-1234567";
+	final static long ID_6 = 6l;
+	final static String NAME_6 = "name6";
+	final static String PONE_6 = "056-1234567";
+	final static long ID_7 = 7l;
+	final static String NAME_7 = "name7";
+	final static String PONE_7= "057-1234567";
+	final static String SUBJECT_1 = "subject1";
+	final static String SUBJECT_2 = "subject2";
+	final static String SUBJECT_3 = "subject3";
+	final static String SUBJECT_4 = "subject4";
+	private static final LocalDate DATE_1 = LocalDate.parse("2023-10-20");
+	private static final LocalDate DATE_2 = LocalDate.parse("2023-11-20");
+	private static final LocalDate DATE_3 = LocalDate.parse("2023-12-20");
+	private static final LocalDate DATE_4 = LocalDate.parse("2024-01-01");
+	Student[] students = {
+		new Student(ID_1, NAME_1, PONE_1),
+		new Student(ID_2, NAME_2, PONE_2),
+		new Student(ID_3, NAME_3, PONE_3),
+		new Student(ID_4, NAME_4, PONE_4),
+		new Student(ID_5, NAME_5, PONE_5),
+		new Student(ID_6, NAME_6, PONE_6),
+		new Student(ID_7, NAME_7, PONE_7),
 		
-		{}
-};
- public void createDB() {
-	 studentRepo.deleteAll();
-	 List<StudentDoc> studentDocs = 
-			IntStream
-			.range(0,students.length)
-			.mapToObj(this::indexToStudent)
-			.toList();
-	 studentRepo.saveAll(studentDocs);
+	};
+	Mark[][] marks = {
+			{
+				new Mark(SUBJECT_1, DATE_1, 80 ),
+				new Mark(SUBJECT_1, DATE_2, 90 ),
+				new Mark(SUBJECT_2, DATE_2, 70 ),
+			},
+			{
+				new Mark(SUBJECT_3, DATE_1, 70 ),
+			},
+			{
+				new Mark(SUBJECT_1, DATE_1, 80 ),
+				new Mark(SUBJECT_4, DATE_3, 70 ),
+			},
+			{
+				new Mark(SUBJECT_2, DATE_1, 100 ),
+				new Mark(SUBJECT_3, DATE_3, 90 ),
+				new Mark(SUBJECT_4, DATE_4, 90 ),
+			},
+			{
+				new Mark(SUBJECT_1, DATE_1, 70 ),
+				new Mark(SUBJECT_3, DATE_3, 70 ),
+				
+			},
+			{
+				new Mark(SUBJECT_1, DATE_1, 100 ),
+				new Mark(SUBJECT_2, DATE_2, 100 ),
+				new Mark(SUBJECT_3, DATE_3, 100 ),
+				new Mark(SUBJECT_4, DATE_4, 100 ),
+			},
 			
- }
- public Mark[] getStudentMarks(long id) {
-	 return marks[(int) (id-1)];
- }
- StudentDoc indexToStudent(int index) {
-	 StudentDoc res = StudentDoc.of(students[index]);
-	 for(Mark mark: marks[index]) {
-		 res.addMark(mark);
-	 }
-	 return res;
- }
+			{}
+	};
+	public void createDB() {
+		studentRepo.deleteAll();
+		List<StudentDoc> studentDocs = IntStream.range(0, students.length).mapToObj(this::indexToStudent).toList();
+		studentRepo.saveAll(studentDocs);
+	}
+	public Mark[] getStudentMarks(long id) {
+		return marks[(int) (id - 1)];
+	}
+	StudentDoc indexToStudent(int index) {
+		StudentDoc res = StudentDoc.of(students[index]);
+		for(Mark mark: marks[index]) {
+			res.addMark(mark);
+		}
+		return res;
+	}
+	public Student getStudent(long id) {
+		return students[(int) (id -1)];
+	}
+ 
+ /*
 public Student getStudent(long id) {
 		return students[(int) (id-1)];
 }
+*/
 }
--- students-marks/src/test/java/telran/students/StudentsServiceTests.java
+++ students-marks/src/test/java/telran/students/StudentsServiceTests.java
@@ -3,11 +3,9 @@ package telran.students;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,23 +13,67 @@ import org.springframework.boot.test.context.SpringBootTest;
 import telran.exceptions.NotFoundException;
 import telran.students.dto.Mark;
 import telran.students.dto.Student;
+import telran.students.repo.StudentRepo;
 import telran.students.service.StudentsService;
 @SpringBootTest
 class StudentsServiceTests {
-	private static final String SERVICE_TEST = "Service: ";
-	private static final long TEST_ID = 12345l;
-	private static final String TEST_PHONE = "058-12345678";
-	private static final Mark markTest = new Mark(DbTestCreation.SUBJECT_1, LocalDate.parse("2024-01-05"), 50 );
-	
 	@Autowired
 StudentsService studentsService;
 	@Autowired
 	DbTestCreation dbCreation;
+	@Autowired
+StudentRepo studentRepo;	
 	@BeforeEach
 	void setUp() {
 		dbCreation.createDB();
 	}
+	@Test
+	void getMarksTest() {
+		Mark[] marksActual = studentsService.getMarks(1).toArray(Mark[]::new);
+		Mark[] marksExpected = dbCreation.getStudentMarks(1);
+		assertArrayEquals(marksExpected, marksActual);
+		assertThrowsExactly(NotFoundException.class, () -> studentsService.getMarks(10000000));
+	}
+	@Test
+	void removeStudentTest() {
+		assertEquals(dbCreation.getStudent(1), studentsService.removeStudent(1));
+		assertThrowsExactly(NotFoundException.class, ()->studentsService.removeStudent(1));
+	}
+	@Test
+	void addMarkTest() {
+		List<Mark> marksStudent2Expected = new ArrayList<>(Arrays.asList(dbCreation.getStudentMarks(2)));
+		Mark mark = new Mark("Java", LocalDate.now(), 100);
+		marksStudent2Expected.add(mark);
+		assertIterableEquals(marksStudent2Expected, studentsService.addMark(2, mark));
+		assertIterableEquals(marksStudent2Expected, studentsService.getMarks(2));
+		assertThrowsExactly(NotFoundException.class, () -> studentsService.addMark(0, mark));
+	}
 	
+	@Test
+	void updatePhoneTest() {
+		Student student1 = dbCreation.getStudent(3);
+		String newPhone = "055-5555555";
+		Student expected = new Student(student1.id(), student1.name(), newPhone);
+		assertEquals(expected, studentsService.updatePhone(3, newPhone));
+		Student actual = studentRepo.findById(3l).orElseThrow().build();
+		assertEquals(expected, actual);
+		assertThrowsExactly(NotFoundException.class, () -> studentsService.updatePhone(0, newPhone));
+	}
+	@Test
+	void addStudentTest() {
+		Student studentExisting = dbCreation.getStudent(4);
+		Student newStudent = new Student(-1l, "Vasya", "111111111");
+		assertEquals(newStudent, studentsService.addStudent(newStudent));
+		Student actual = studentRepo.findById(-1l).orElseThrow().build();
+		assertEquals(newStudent, actual);
+		assertThrowsExactly(IllegalStateException.class,
+				()-> studentsService.addStudent(studentExisting));
+	}
+
+
+}
+	
+	/* This is my code
 	@Test
 	@DisplayName(SERVICE_TEST + TestDisplayNames.GET_MARKS_NORMAL)
 	void getMarksTest() {
@@ -106,6 +148,7 @@ StudentsService studentsService;
 		assertThrowsExactly(NotFoundException.class,
 				()-> studentsService.removeStudent(TEST_ID));
 	}
+	*/
 	
 
-}
+
